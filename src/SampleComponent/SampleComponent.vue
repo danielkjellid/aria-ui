3b3bfe40d6d7493040e1dd9d40e4accc8c851b68
danielkjellid <daniel.kjellid@gmail.com>
2021-11-28 15:16:23 +0100

    Initial commit: project structure

---
+++ src/SampleComponent/SampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <button>This is a button with text: {{ sampleText }}</button>
+</template>
+
+<script>
+export default {
+  setup() {
+    let sampleText = 'WOOOO!'
+
+    return {
+      sampleText
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
