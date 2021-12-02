2d5044ca48c9c59a2b62d3bdae8b59dc165b1d9f
danielkjellid <daniel.kjellid@gmail.com>
2021-12-02 15:28:22 +0100

    Added error banner component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import './tailwind.css'
 
+import Breadcrumbs from './Breadcrumbs/Breadcrumbs.vue'
+export { Breadcrumbs }
+
 import Button from './Button/Button.vue'
 export { Button }
 
 import Checkbox from './CheckBox/Checkbox.vue'
 export { Checkbox }
+
+import ErrorBanner from './ErrorBanner/ErrorBanner.vue'
+export { ErrorBanner }
