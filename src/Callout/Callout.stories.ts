08ce37859d24d04fe1599729abc8ad662be14d8b
danielkjellid <daniel.kjellid@gmail.com>
2021-12-20 16:51:48 +0100

    Add story source code to callouts

--- src/Callout/Callout.stories.ts
+++ src/Callout/Callout.stories.ts
@@ -42,15 +42,75 @@ const Template: Story<any> = (args: any) => ({
 })
 
 export const API = Template.bind({})
+API.parameters = {
+  docs: {
+    source: {
+      code: 
+`<Callout 
+  variant="danger" 
+  message="Oida! Noe gikk galt"
+  :context="['One bullet point', 'Another bullet point']"
+/>`
+    }
+  }
+}
 
 export const Danger = Template.bind({})
 Danger.args = { message: 'Oida! Noe gikk galt', variant: 'danger', context: ['One bullet point', 'Another bullet point']}
+Danger.parameters = {
+  docs: {
+    source: {
+      code: 
+`<Callout 
+  variant="danger" 
+  message="Oida! Noe gikk galt"
+  :context="['One bullet point', 'Another bullet point']"
+/>`
+    }
+  }
+}
 
 export const Warning = Template.bind({})
 Warning.args = { message: 'Obs! Vær oppmerksom på', variant: 'warning', context: ['One bullet point', 'Another bullet point']}
+Warning.parameters = {
+  docs: {
+    source: {
+      code: 
+`<Callout 
+  variant="warning" 
+  message="Obs! Vær oppmerksom på"
+  :context="['One bullet point', 'Another bullet point']"
+/>`
+    }
+  }
+}
 
 export const Info = Template.bind({})
 Info.args = { message: 'Visste du at', variant: 'info', context: ['One bullet point', 'Another bullet point']}
+Info.parameters = {
+  docs: {
+    source: {
+      code: 
+`<Callout 
+  variant="info" 
+  message="Visste du at"
+  :context="['One bullet point', 'Another bullet point']"
+/>`
+    }
+  }
+}
 
 export const Success = Template.bind({})
 Success.args = { message: 'Oppdatering vellykket', variant: 'success', context: ['One bullet point', 'Another bullet point']}
+Success.parameters = {
+  docs: {
+    source: {
+      code: 
+`<Callout 
+  variant="success" 
+  message="Oppdatering vellykket"
+  :context="['One bullet point', 'Another bullet point']"
+/>`
+    }
+  }
+}
