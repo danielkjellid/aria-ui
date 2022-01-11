14c193cbd601ac48a39d256d49fd353af0b692c4
danielkjellid <daniel.kjellid@gmail.com>
2022-01-11 22:23:43 +0100

    Update eslint, make prettier and eslint play well and migrate storybook to postcss build

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,14 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-backgrounds',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: '@storybook/vue3',
 }
