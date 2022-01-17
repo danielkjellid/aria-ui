4c6f0c8ed1b16a54d0a99075c53f0b92e542ddca
danielkjellid <daniel.kjellid@gmail.com>
2022-01-17 10:57:46 +0100

    Add stories to link component

---
+++ src/components/Link/Link.stories.ts
@@ -0,0 +1,28 @@
+import { Meta, Story } from '@storybook/vue3'
+import ALink from './Link.vue'
+import { LinkProps } from './types'
+
+export default {
+  title: 'UI Elements/Link',
+  component: ALink,
+  parameter: {
+    viewMode: 'docs',
+  },
+} as Meta
+
+const Template: Story<LinkProps> = (args: LinkProps) => ({
+  components: { ALink },
+  setup() {
+    return { args }
+  },
+  template: '<a-link v-bind="args">Example link</a-link>',
+})
+
+export const API = Template.bind({})
+API.parameters = {
+  docs: {
+    source: {
+      code: '<a-link tag="a" href="#">Example link</a-link>',
+    },
+  },
+}
--- src/components/Link/Link.vue
+++ src/components/Link/Link.vue
@@ -2,20 +2,21 @@
   <component
     :is="tag"
     v-bind="$attrs"
-    class="decoration-brand-900 underline-offset-2 hover:underline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-800 focus:border-transparent hover:text-brand-800 inline-flex items-center flex-shrink-0 text-sm font-medium text-gray-900 rounded-sm appearance-none"
+    class="decoration-brand-900 underline-offset-2 hover:underline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-800 focus:border-transparent hover:text-brand-800 inline-flex items-center flex-shrink-0 text-sm font-medium text-gray-900 rounded-sm appearance-none cursor-pointer"
   >
     <slot /> →
   </component>
 </template>
 
 <script lang="ts">
-import { ArrowRightIcon } from '@heroicons/vue/solid'
 import config from '../../utils/config'
 
 export default {
   name: 'ALink',
-  components: { ArrowRightIcon },
   props: {
+    /**
+     * Which tag to render the link as. Defaults to 'a', or whatever is set in the plugin config.
+     */
     tag: {
       type: String,
       required: true,
