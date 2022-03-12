a6f3e5e7842e48c2a689a82da1700f2cf053fe4c
danielkjellid <daniel.kjellid@gmail.com>
2022-03-12 22:15:18 +0100

    Export all navbar components

--- src/components/Navbar/index.ts
+++ src/components/Navbar/index.ts
@@ -1,11 +1,19 @@
 import { App as Application } from 'vue'
 import Navbar from './Navbar.vue'
+import FlyoutMenu from "./FlyoutMenu.vue"
+import MobileMenu from "./MobileMenu.vue"
+import NavbarItem from "./NavbarItem.vue"
+import NavbarItemFlyout from "./NavbarItemFlyout.vue"
 
 import { use, registerComponent } from '../../utils/plugins'
 
 const Plugin = {
   install(instance: Application) {
     registerComponent(instance, Navbar)
+    registerComponent(instance, FlyoutMenu)
+    registerComponent(instance, MobileMenu)
+    registerComponent(instance, NavbarItem)
+    registerComponent(instance, NavbarItemFlyout)
   },
 }
 
